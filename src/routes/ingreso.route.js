b6c3ce877420c08db879c951015312df97870890
dlmateogarcialopez <dl.mgarcia@umanizales.edu.co>
2023-02-16 09:02:00 -0500

    ruta de get a post para ingerso y registro

--- src/routes/ingreso.route.js
+++ src/routes/ingreso.route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const ctrlIngreso = require('../controllers/ingreso.controller')
 const validations = require('../utils/validations')
 
-
+//...
 router.post('/ingreso', validations.validar_ingreso, ctrlIngreso.ingreso)
 
 module.exports = router
